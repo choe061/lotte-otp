88eb2b72255377efb8c94ca3d77f363ed1fbbf2a
Choe <qjarbs111@gmail.com>
2018-01-27 23:34:53 +0900

    DatabaseConfig 추가

---
+++ src/main/java/com/lotte/otp/config/DatabaseConfig.java
@@ -0,0 +1,54 @@
+package com.lotte.otp.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.TypeHandler;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by choi on 2018. 1. 27. PM 11:02.
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.lotte.otp.repository"})
+public class DatabaseConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new DataSource();
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setTypeAliasesPackage("com.lotte.otp.entity");
+        sqlSessionFactory.setMapperLocations(applicationContext.getResources("classpath:mapper/*-mapper.xml"));
+
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
--- src/main/java/com/lotte/otp/controller/UserController.java
+++ src/main/java/com/lotte/otp/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.lotte.otp.controller;
 
 import com.lotte.otp.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by choi on 2018. 1. 26. PM 2:57.
  */
-@RestController("/user")
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login")
     public String login() {
         return "login";
     }
