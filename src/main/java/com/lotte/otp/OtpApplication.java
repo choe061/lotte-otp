401a5de620b8e5c1e3f68737b02716115c4216b3
Choe <qjarbs111@gmail.com>
2018-02-02 10:54:53 +0900

    springboot-app.pid 파일 생성 로직 추가

--- src/main/java/com/lotte/otp/OtpApplication.java
+++ src/main/java/com/lotte/otp/OtpApplication.java
@@ -2,11 +2,14 @@ package com.lotte.otp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class OtpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OtpApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(OtpApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 }
