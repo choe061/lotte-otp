92114f6adbc7ef695009f22ed45548ec2fd0b7bc
Choe <qjarbs111@gmail.com>
2018-02-13 11:13:41 +0900

    프론트 수정

--- src/main/resources/static/js/sign-up.js
+++ src/main/resources/static/js/sign-up.js
@@ -10,25 +10,24 @@ var duplicateId = function (id) {
             method: "GET",
             type: "json",
             success: function (data) {
-                console.log(data.result);
-                alert(data.result);
+                alert("사용 가능한 아이디 입니다.");
                 console.log("%cSuccess%c!!!", "color: blue", "color: red");
             },
             error: function (request, status, error) {
                 if (status === 409) {
-                    alert("이미 존재하는 아이디입니다.")
+                    alert("이미 존재하는 아이디입니다!")
                 } else {
-                    alert("error");
+                    alert("다시 시도해주세요.");
                     console.log("%cError%c!!!", "color: blue", "color: red");
                 }
             }
         });
     } else {
-        alert("아이디를 다시 입력하세요. (영문,숫자 조합 6자 이상, 20자 이하)")
+        alert("아이디를 다시 입력하세요. (영문,숫자 조합 5자 이상, 20자 이하)")
     }
 };
 
 var isValidId = function (id) {
-    var pattern = new RegExp("^[a-zA-Z][a-zA-Z0-9]{5,19}$");    //ID의 첫번째 글자는 반드시 영문으로 시작 이후는 영문+숫자 조합
+    var pattern = new RegExp("^[a-zA-Z][a-zA-Z0-9]{4,20}$");    //ID의 첫번째 글자는 반드시 영문으로 시작 이후는 영문+숫자 조합
     return pattern.test(id);
 };
